ef48ad38313ac2b721376fd69cdf70c808c815a1
LuK1337 <priv.luk@gmail.com>
2021-08-30 12:28:33 +0200

    sdm845-common: Override vendor OdmOverlay-OPSystemUI
    Change-Id: Ic3a3f6ae5323ae1ca4e8e9c36b956638611881c3

--- common.mk
+++ common.mk
@@ -29,6 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-nad
 
 PRODUCT_PACKAGES += \
+    OdmOverlay-OPSystemUI \
     OnePlusIconShapeCircleOverlay \
     OnePlusIconShapeRoundedRectOverlay \
     OnePlusIconShapeSquareOverlay \
